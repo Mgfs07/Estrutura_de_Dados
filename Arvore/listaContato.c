fa4fb1fa1bc6c5522348b49e1832580126ffe1a6
Mgfs07 <matheusantos07@hotmail.com>
2022-06-06 20:21:33 -0300

    criando função para poder iniciar a Arvore

--- Arvore/listaContato.c
+++ Arvore/listaContato.c
@@ -4,108 +4,107 @@
 
 #include "listaContato.h"
 
-void iniciaLista(TListaContato * lista) {
-    lista->inicio = NULL;
-    lista->fim = NULL;
+void iniciaLista(TElementoContato ** arvore) {
+    *arvore = NULL;
 }
 
-TElementoContato * insereListaContato(TListaContato * lista, char * nome, long numeroTelefone){
-    TElementoContato *novo = (TElementoContato *) malloc(sizeof(TElementoContato));
-    TElementoContato * atual;
-    strcpy(novo->nome, nome);
-    novo->telefone = numeroTelefone;
-    if(lista->inicio == NULL){
-        novo->prox = NULL;
-        lista->inicio = novo;
-        lista->fim = novo;
-        novo->ant = NULL;
-    }else {
-        atual = lista->inicio;
-        while ((atual != NULL) && (strcmp(atual->nome, nome) < 0))
-            atual = atual->prox;
-        if (atual == lista->inicio) {
-            novo->ant = NULL;
-            novo->prox = atual;
-            atual->ant = novo;
-            lista->inicio = novo;
-        } else if (atual == NULL) {
-            novo->ant = lista->fim;
-            novo->prox = NULL;
-            lista->fim->prox = novo;
-            lista->fim = novo;
-        } else {
-            novo->prox = atual;
-            novo->ant = atual->ant;
-            atual->ant->prox = novo;
-            atual->ant = novo;
-        }
-    }
-    return novo;
-
-}
-
-int listaVazia(TListaContato f) {
-    if (f.inicio == NULL)
-        return 1;
-    else
-        return 0;
-}
-
-//metodo de excluir o contato
-int excluirContatoNome(TListaContato * lista, char * nome){
-    TElementoContato *atual, *anterior;
-    anterior = NULL;
-    int excluido = 0;
-    atual = buscarPorNome(*lista, nome);
-    if(listaVazia(*lista) == 1){
-        printf("FILA ESTA VAZIA, NAO PODE EXCLUIR NINGUEM");
-    }else{
-        if(atual){
-            excluido = 1;
-            anterior = atual->ant;
-            if(anterior == NULL) lista->inicio = atual->prox;
-            else anterior->prox = atual->prox;
-
-            if(atual == lista->fim) lista->fim = NULL;
-            free(atual);
-        }
-    }
-    return excluido;
-}
-
-TElementoContato * buscarPorNome(TListaContato listaContato, char * nome){
-    TElementoContato * atual;
-    atual = listaContato.inicio;
-    while ((atual != NULL) && (strcmp(nome,atual->nome)>0)) {
-        atual = atual->prox;
-    }
-    if(atual != NULL && (strcmp(nome,atual->nome)==0)){
-        return atual;
-    }else{
-        return NULL;
-    }
-}
-
-TElementoContato * buscarNumero(TListaContato listaContato, long telefone){
-    TElementoContato * atual;
-    atual = listaContato.inicio;
-    while ((atual != NULL) && (atual->telefone != telefone)){
-        atual = atual->prox;
-    }
-    if(atual != NULL && atual->telefone == telefone){
-        return atual;
-    }else{
-        return NULL;
-    }
-}
-
-void listarTodos(TListaContato *lista){
-    TElementoContato * atual;
-    atual = lista->inicio;
-    while (atual != NULL){
-        printf("\n\nNome -> %s", atual->nome);
-        printf("\n\nTelefone -> %ld,", atual->telefone);
-        atual = atual->prox;
-        printf("\n\n=================\n\n");
-    }
-}
+//TElementoContato * insereListaContato(TElementoContato * lista, char * nome, long numeroTelefone){
+//    TElementoContato *novo = (TElementoContato *) malloc(sizeof(TElementoContato));
+//    TElementoContato * atual;
+//    strcpy(novo->nome, nome);
+//    novo->telefone = numeroTelefone;
+//    if(lista->inicio == NULL){
+//        novo->direita = NULL;
+//        lista->inicio = novo;
+//        lista->fim = novo;
+//        novo->esquerda = NULL;
+//    }else {
+//        atual = lista->inicio;
+//        while ((atual != NULL) && (strcmp(atual->nome, nome) < 0))
+//            atual = atual->direita;
+//        if (atual == lista->inicio) {
+//            novo->esquerda = NULL;
+//            novo->direita = atual;
+//            atual->esquerda = novo;
+//            lista->inicio = novo;
+//        } else if (atual == NULL) {
+//            novo->esquerda = lista->fim;
+//            novo->direita = NULL;
+//            lista->fim->direita = novo;
+//            lista->fim = novo;
+//        } else {
+//            novo->direita = atual;
+//            novo->esquerda = atual->esquerda;
+//            atual->esquerda->direita = novo;
+//            atual->esquerda = novo;
+//        }
+//    }
+//    return novo;
+//
+//}
+//
+//int listaVazia(TElementoContato f) {
+//    if (f.inicio == NULL)
+//        return 1;
+//    else
+//        return 0;
+//}
+//
+////metodo de excluir o contato
+//int excluirContatoNome(TListaContato * lista, char * nome){
+//    TElementoContato *atual, *anterior;
+//    anterior = NULL;
+//    int excluido = 0;
+//    atual = buscarPorNome(*lista, nome);
+//    if(listaVazia(*lista) == 1){
+//        printf("FILA ESTA VAZIA, NAO PODE EXCLUIR NINGUEM");
+//    }else{
+//        if(atual){
+//            excluido = 1;
+//            anterior = atual->esquerda;
+//            if(anterior == NULL) lista->inicio = atual->direita;
+//            else anterior->direita = atual->direita;
+//
+//            if(atual == lista->fim) lista->fim = NULL;
+//            free(atual);
+//        }
+//    }
+//    return excluido;
+//}
+//
+//TElementoContato * buscarPorNome(TListaContato arvore, char * nome){
+//    TElementoContato * atual;
+//    atual = arvore.inicio;
+//    while ((atual != NULL) && (strcmp(nome,atual->nome)>0)) {
+//        atual = atual->direita;
+//    }
+//    if(atual != NULL && (strcmp(nome,atual->nome)==0)){
+//        return atual;
+//    }else{
+//        return NULL;
+//    }
+//}
+//
+//TElementoContato * buscarNumero(TListaContato listaContato, long telefone){
+//    TElementoContato * atual;
+//    atual = listaContato.inicio;
+//    while ((atual != NULL) && (atual->telefone != telefone)){
+//        atual = atual->direita;
+//    }
+//    if(atual != NULL && atual->telefone == telefone){
+//        return atual;
+//    }else{
+//        return NULL;
+//    }
+//}
+//
+//void listarTodos(TListaContato *lista){
+//    TElementoContato * atual;
+//    atual = lista->inicio;
+//    while (atual != NULL){
+//        printf("\n\nNome -> %s", atual->nome);
+//        printf("\n\nTelefone -> %ld,", atual->telefone);
+//        atual = atual->direita;
+//        printf("\n\n=================\n\n");
+//    }
+//}
--- Arvore/main.c
+++ Arvore/main.c
@@ -3,13 +3,12 @@
 
 int main() {
 
-    TListaContato lista;
-    iniciaLista(&lista);
+    TElementoContato * arvore;
+    iniciaLista(&arvore);
 
     int valorMenu;
     long numeroTelefone;
     char nome[TAMANHO];
-    TElementoContato *contato;
 
     do {
         printf("\nOpcoes: \n");
@@ -27,46 +26,14 @@ int main() {
             case 0:
                 break;
             case 1:
-                printf("\nNome do cliente: ");
-                gets(nome);
-                contato = buscarPorNome(lista, nome);
-                if (!contato) {
-                    printf("insira o numero que deseja: ");
-                    scanf("%ld", &numeroTelefone);
-                    insereListaContato(&lista, nome, numeroTelefone);
-                    printf("CONTATO INSERIDO COM SUCESSO\n\n");
-                } else {
-                    printf("\nContato já existe\n");
-                    printf("Nome -> %s / Telefone -> %ld", contato->nome, contato->telefone);
-                }
                 break;
             case 2:
-                printf("insira o nome que deseja excluir: ");
-                gets(nome);
-                if (excluirContatoNome(&lista, nome) == 1) {
-                    printf("\nContato excluido com sucesso\n");
-                } else {
-                    printf("\nContato nao encontrado para ser excluido\n");
-                }
                 break;
             case 3:
-                listarTodos(&lista);
                 break;
             case 4:
-                printf("qual o nome que deseja procurar: ");
-                gets(nome);
-                contato = buscarPorNome(lista, nome);
-                !contato ? printf("\n\nNÃO EXISTE CONTATO EXISTENTE") : printf("\nContato existe\n"
-                                                                               "Nome -> %s\n"
-                                                                               "Telefone -> %ld\n\n", contato->nome,
-                                                                               contato->telefone);
                 break;
             case 5:
-                printf("qual numero deseja procurar: ");
-                scanf("%ld", &numeroTelefone);
-                contato = buscarNumero(lista, numeroTelefone);
-                !contato ? printf("\nNUMERO DESCONHECIDO") : printf("\nO CONTATO -> %ld pertence ao NOME -> %s\n",
-                                                                    contato->telefone, contato->nome);
                 break;
             default:
                 printf("Valor incorreto\n");
